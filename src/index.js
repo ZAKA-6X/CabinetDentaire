d0bdddf48e1fc1c5e9c285c66054a649e8239cf5
Habiba <Habibaelabed2004@gmail.com>
2026-04-21 15:32:02 +0100

    ma version

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <h1>hello world</h1>
+    <h1>Habiba</h1>
   </div>
 );
 
