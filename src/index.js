664932e59743d47704fcdd54f44b41e40fb1f5ed
Habiba <Habibaelabed2004@gmail.com>
2026-04-21 15:41:54 +0100

    ma version de ma propre branch

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <h1>Habiba</h1>
+    <h1>ma version de ma propre branche</h1>
   </div>
 );
 
